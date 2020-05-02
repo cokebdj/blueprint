c703c53605d4a11bb70e218dabe606d4f48f69ff
coke_bdj <alvarojaramillogarcia@gmail.com>
2020-05-02 21:42:33 +0200

    tests updated (faked one)

--- conftest.py
+++ conftest.py
@@ -1,6 +1,17 @@
 import pytest
+from ml_api.app import flask_app
 
 @pytest.fixture
 def input_value():
     return 4
+
+@pytest.fixture
+def client():
+    return flask_app.test_client()
+
+@pytest.fixture
+def input_dict():
+    dictionary = dict()
+    dictionary['number'] = 2
+    return dictionary
 
---
+++ tests/test_ml_api.py
@@ -0,0 +1,14 @@
+# from mock import patch
+from ml_api.app import  healthcheck
+import json
+
+def test_healthcheck_endpoint():
+    _, status_code = healthcheck()
+    assert status_code == 200
+
+def test_correct_endpoint(client,input_dict):
+    response = client.post(data=json.dumps(input_dict))
+    assert response.status_code == 404
+
+# patch("",class).start()
+# patch.stopall()
